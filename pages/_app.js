98f2523864defbfd02beef0d73649877d037a9c5
thedanielmark <danielmark.uc@gmail.com>
2020-12-20 20:59:56 +0530

    Added new service worker

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
-import '../styles/globals.css';
-import '../styles/bootstrap.min.css';
+import "../styles/globals.css";
+import "../styles/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    self.addEventListener("install", function (e) {
+      self.skipWaiting();
+    });
+
+    self.addEventListener("activate", function (e) {
+      self.registration
+        .unregister()
+        .then(function () {
+          return self.clients.matchAll();
+        })
+        .then(function (clients) {
+          clients.forEach((client) => client.navigate(client.url));
+        });
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
