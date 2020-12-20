ff7dba18977cb3f8cfd9be6154ec7cdf37c54511
thedanielmark <danielmark.uc@gmail.com>
2020-12-20 10:33:30 +0530

    Added new links

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div
@@ -9,6 +11,38 @@ export default function Home() {
         <h5 className="d-block text-grey">
           I build enterprise systems and scalable apps.
         </h5>
+        <div>
+          <ul className="nav ml-0 mt-4">
+            <li className="nav-item">
+              <Link href={"/work"}>
+                <a
+                  className="nav-link grey-to-white"
+                  style={{ paddingLeft: "0 !important" }}
+                >
+                  Work
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link grey-to-white"
+                target="top"
+                href="https://linkedin.com/in/thedanielmark"
+              >
+                Personal
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link grey-to-white"
+                target="top"
+                href="https://twitter.com/the_danielmark"
+              >
+                Other fun stuff
+              </a>
+            </li>
+          </ul>
+        </div>
         <div>
           <ul className="nav ml-0 mt-4">
             <li className="nav-item">
@@ -41,36 +75,6 @@ export default function Home() {
             </li>
           </ul>
         </div>
-        <ul className="nav ml-0 mt-4">
-          <li className="nav-item">
-            <a
-              className="nav-link grey-to-white"
-              style={{ paddingLeft: "0 !important" }}
-              target="top"
-              href="https://github.com/thedanielmark"
-            >
-              <i className="fab fa-github" />
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link grey-to-white"
-              target="top"
-              href="https://linkedin.com/in/thedanielmark"
-            >
-              <i className="fab fa-linkedin-in" />
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link grey-to-white"
-              target="top"
-              href="https://twitter.com/the_danielmark"
-            >
-              <i className="fab fa-twitter" />
-            </a>
-          </li>
-        </ul>
       </div>
     </div>
   );
