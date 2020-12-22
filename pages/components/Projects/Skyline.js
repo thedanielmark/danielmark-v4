79f75f777b68254ef71989fab500675b41d7c66b
thedanielmark <danielmark.uc@gmail.com>
2020-12-23 02:36:18 +0530

    Added JailBird

--- pages/components/Projects/Skyline.js
+++ pages/components/Projects/Skyline.js
@@ -21,94 +21,27 @@ export default function Skyline() {
         <hr className="mt-4" />
         <div>
           <div>
-            <h6 className="middle-underline d-inline">
-              Skyline
-            </h6>
+            <h6 className="middle-underline d-inline">Skyline</h6>
           </div>
           <p className="mt-2">
-            MixSpace is an internet company that is a cloud computing platform with a focus on serverless
-            computing applications.
+            Skyline was a social media site like Twitter where users could make
+            posts, interact with other users and add them as friends as well as
+            upload media content.
           </p>
           <h6 className="mt-4">Releases</h6>
           <div id="tech-badges">
-            <ul className="nav">
-              <li className="nav-item">
-                <Link passHref href={"https://v1.mixspace.xyz/"}>
-                  <a
-                    className="nav-link grey-to-white"
-                    style={{ paddingLeft: "0px" }}
-                  >
-                    v1
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link passHref href={"https://v2.mixspace.xyz/"}>
-                  <a
-                    className="nav-link grey-to-white"
-                    style={{ paddingLeft: "0px" }}
-                  >
-                    v2
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link passHref href={"https://v3.mixspace.xyz/"}>
-                  <a
-                    className="nav-link grey-to-white"
-                    style={{ paddingLeft: "0px" }}
-                  >
-                    v3
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link passHref href={"https://v4.mixspace.xyz/"}>
-                  <a
-                    className="nav-link grey-to-white"
-                    style={{ paddingLeft: "0px" }}
-                  >
-                    v4
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link passHref href={"https://v5.mixspace.xyz/"}>
-                  <a
-                    className="nav-link grey-to-white"
-                    style={{ paddingLeft: "0px" }}
-                  >
-                    v5 (latest)
-                  </a>
-                </Link>
-              </li>
-            </ul>
+            <Link passHref href={""}>
+              <a className="grey-to-white text-decoration-none">v1 (latest)</a>
+            </Link>
           </div>
           <h6 className="mt-4">Technologies</h6>
           <div id="tech-badges">
-            <span className="badge rounded-pill bg-green">Next.js</span>
-            <span className="badge rounded-pill bg-green">React</span>
             <span className="badge rounded-pill bg-green">PHP</span>
-            <span className="badge rounded-pill bg-green">Python</span>
             <span className="badge rounded-pill bg-green">Apache</span>
-            <span className="badge rounded-pill bg-green">Nginx</span>
-            <span className="badge rounded-pill bg-green">Shell</span>
-            <span className="badge rounded-pill bg-green">Node.js</span>
             <span className="badge rounded-pill bg-green">jQuery</span>
             <span className="badge rounded-pill bg-green">Bootstrap</span>
             <span className="badge rounded-pill bg-green">MariaDB</span>
-            <span className="badge rounded-pill bg-green">MongoDB</span>
-            <span className="badge rounded-pill bg-green">Firestore</span>
             <span className="badge rounded-pill bg-green">AJAX</span>
-            <span className="badge rounded-pill bg-green">SWR</span>
-            <span className="badge rounded-pill bg-green">jQuery</span>
-            <span className="badge rounded-pill bg-green">Docker</span>
-            <span className="badge rounded-pill bg-green">Linux</span>
-            <span className="badge rounded-pill bg-green">ESXi</span>
-            <span className="badge rounded-pill bg-green">Git</span>
-            <span className="badge rounded-pill bg-green">OpenStack</span>
-            <span className="badge rounded-pill bg-green">Cloudflare</span>
-            <span className="badge rounded-pill bg-green">Vercel</span>
           </div>
         </div>
       </motion.div>
