e134565c251bf52e2fcc93c108cdebcffc64c5d7
thedanielmark <danielmark.uc@gmail.com>
2021-04-28 21:33:16 +0530

    Fixed firebase bug

--- pages/components/Contact/MainContent.jsx
+++ pages/components/Contact/MainContent.jsx
@@ -5,7 +5,21 @@ import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 import firebase from "firebase/app";
-import "./firebase";
+import "firebase/analytics";
+import "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDzFb8s3QcEowPVLArSWfOHb5myHXf590o",
+  authDomain: "thedanielmark-v4.firebaseapp.com",
+  projectId: "thedanielmark-v4",
+  storageBucket: "thedanielmark-v4.appspot.com",
+  messagingSenderId: "49428790703",
+  appId: "1:49428790703:web:74f726002e83b60b86c35a",
+  measurementId: "G-8SXMJR0K0Q",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   function Alert(props) {
